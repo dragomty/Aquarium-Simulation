649c3ded31f047ede49ac422a1f6ce9192c537d3
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-25 11:11:45 +0300

    Update IBehaviour.java

--- IBehaviour.java
+++ IBehaviour.java
@@ -10,10 +10,8 @@ package Behaviours;
 public interface IBehaviour
 {
     /**
-     * An example of a method header - replace this comment with your own
      * 
-     * @param  y    a sample parameter for a method
-     * @return        the result produced by sampleMethod 
+     * 
      */
     public void move();
 }
