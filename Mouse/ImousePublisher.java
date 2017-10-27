55d7bc819d74173d00d5512b9e8e8c0ef1f70df0
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-27 13:40:29 +0300

    Update ImousePublisher.java

--- Mouse/ImousePublisher.java
+++ Mouse/ImousePublisher.java
@@ -2,19 +2,14 @@ package Mouse;
 import env3d.Env; 
 
 /**
- * Write a description of interface ImousePublisher here.
  * 
- * @author (me) 
+ * 
+ * @author Justin
  * @version (0)
  */
 public interface ImousePublisher
 {
-    /**
-     * An example of a method header - replace this comment with your own
-     * 
-     * @param  y    a sample parameter for a method
-     * @return        the result produced by sampleMethod 
-     */
+
     int[] OnMouse(int[] rtnVal);
     void Subscribe(ImouseListener IMouseListener);
     void Unsubscribe(ImouseListener IMouseListener);
