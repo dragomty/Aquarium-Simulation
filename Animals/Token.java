2dcc5065d08372d311f22ed81a3bc633f4bdd466
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-27 13:04:12 +0300

    Update Token.java

--- Animals/Token.java
+++ Animals/Token.java
@@ -3,13 +3,12 @@ package Animals;
 /**
  * Token class that holds its fields.
  * 
- * @author me
+ * @author Justin
  * @version 0
  */
-public class Token implements IToken///i dont want to make this abstract????/
+public class Token implements IToken
 {
     // Instance variables (fields):
-    
     // --- DECLARE instance variables rqd by Env3D:
     // reference to the 3D model:
     private String model = "models/billboard/billboard.obj";
@@ -34,34 +33,30 @@ public class Token implements IToken///i dont want to make this abstract????/
    
     /**
      * Constructor for objects of class Token
-     * constructor has to  take 3 parameters(string to initialise
-     * texture field and two doubles to initialise x and y coordinates)
      * 
      */
     public Token(String ObjectTexture,String model)
     {
-     ////setPosition(xpos,ypos);//changed 27 1--------------*+*-------------+-----++++-++-+-*+--------------------------------------------------------------------------------------------------------------------++++------------------------++------------------------------------------*---
-     texture = ObjectTexture;///this is IMPORTANT!!!!!!!!
+    
+     texture = ObjectTexture;
  
     }
     
     public Token(String ObjectTexture, double xpos, double ypos, String SphereModel)
     {
-        texture = ObjectTexture;///this is IMPORTANT!!!!!!!!
+        texture = ObjectTexture;
         model = SphereModel;
-        this.x = xpos;//you need these 2 from setposition method to make it nice and clean
-        this.y = ypos;//you need these 2 from setposition method to make it nice and cl
+        this.x = xpos;
+        this.y = ypos;
     }
 
     /**
      * METHOD: Place the token at the given orientation within the aquarium
      *
-     * @param  coords   the new orientation (x,y,z) of the token as an array of three Doubles
      */
-    public void orientation(Double ...coordinates)//taken from week 11
+    public void orientation(Double ...coordinates)
     {
-        // VALIDATE coords:
-
+        
         // SET the new orientation:
         rotateX = coordinates[0];
         rotateY = coordinates[1];
@@ -84,10 +79,6 @@ public class Token implements IToken///i dont want to make this abstract????/
         this.rotateY = yRot;
         this.rotateZ = zRot;
        
-        //this.x += _speed;
     }
     
-   
-    
-
 }
