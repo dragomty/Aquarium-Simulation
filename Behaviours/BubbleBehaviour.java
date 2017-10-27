4c9a2635057621eb291f21a69896f7af9b26f71b
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-27 13:14:52 +0300

    Update BubbleBehaviour.java

--- Behaviours/BubbleBehaviour.java
+++ Behaviours/BubbleBehaviour.java
@@ -1,10 +1,10 @@
 package Behaviours;
 import Animals.IToken;
-import java.util.Random;//import random to randomizes taken from website http://www.javapractices.com/topic/TopicAction.do?Id=62
+import java.util.Random;
 import java.lang.String;
 
 /**
- * A class for the javafish behaviour. Responsible for movement and positioning.
+ * A class for the bubble behaviour. Responsible for movement and positioning.
  * 
  * @author Justin
  * @version 0
@@ -12,8 +12,7 @@ import java.lang.String;
  */
 public class BubbleBehaviour extends Behaviour
 {
-    // instance variables - replace the example below with your own
- 
+   
    // position in to be placed at in 3D world (x,y,z coordinates):
    private double x=1.0, y=1.0, z=1.0;
    
@@ -31,8 +30,6 @@ public class BubbleBehaviour extends Behaviour
     {
         super(BehaviourToken);///calling parent class
         // initialise instance variables
-        //this.x = _randomPos;///////////
-        //this.y = _randomPos;////////////////////
         this.rotateX = xRot;
         this.rotateY = yRot;
         this.rotateZ = zRot;
@@ -50,8 +47,6 @@ public class BubbleBehaviour extends Behaviour
     {
         super(BehaviourToken);///calling parent class
         // initialise instance variables
-        //this.x = _randomPos;///////////
-        //this.y = _randomPos;////////////////////
         this.rotateX = xRot;
         this.rotateY = yRot;
         this.rotateZ = zRot;
@@ -67,7 +62,7 @@ public class BubbleBehaviour extends Behaviour
      * 
      * 
      */
-     public Double[] position()///taken from week 11
+     public Double[] position()
     {
         Double positionVal[] = {x, y, z};
         return positionVal;
@@ -83,30 +78,12 @@ public class BubbleBehaviour extends Behaviour
         return _speed;
     }
     
-    /**
-     * making the random position accesible to bubbles
-     * 
-     */
-    /*public double BrndPosition()
-    {
-        double BrndPos = _randomPos;
-        return _randomPos;
-        
-    }*/
-    
   
-    /**
-     * A movement method for bubble
-     * 
-     * @param none
-     * @return none
-     */
-    
     public void move()
     {
-       y += _speed;//applies our speed to x axis
+       y += _speed;
        
-       super.move(x,y,rotateX, rotateY, rotateZ);/////CHECK IT
+       super.move(x,y,rotateX, rotateY, rotateZ);
        System.out.println(_speed);
       
     }
